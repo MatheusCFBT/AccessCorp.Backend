a5696dd7dbd8f6e9324141259fe534201cf14e10
MatheusCFBT <matheusterzini@gmail.com>
2025-03-01 13:48:36 -0300

    Controller is sending JWT

--- AccessCorp.WebApi/Controllers/AuthController.cs
+++ AccessCorp.WebApi/Controllers/AuthController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using OnFunction.Application.Entities;
+using OnFunction.WebApi.Extensions;
 
 namespace OnFunction.WebApi.Controllers;
 
@@ -11,11 +17,15 @@ public class AuthController : Controller
     private readonly ILogger<AuthController> _logger;
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
-
-    public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+    private readonly AppSettings _appSettings;
+    
+    public AuthController(SignInManager<IdentityUser> signInManager, 
+                          UserManager<IdentityUser> userManager,
+                          IOptions<AppSettings> appSettings)
     {
         _signInManager = signInManager;
         _userManager = userManager;
+        _appSettings = appSettings.Value;
     }
     
     [HttpPost("register-administrator")]
@@ -35,7 +45,7 @@ public class AuthController : Controller
         if (result.Succeeded)
         {
             await _signInManager.SignInAsync(user, false);
-            return Ok();
+            return Ok(await GerarJwt(request.Email));
         }
         
         return BadRequest(request);    
@@ -50,9 +60,63 @@ public class AuthController : Controller
 
         if (result.Succeeded)
         {
-            return Ok(request);
+            return Ok(await GerarJwt(request.Email));
         }
         
         return BadRequest(request); 
     }
+
+    private async Task<AdministratorResponseVM> GerarJwt(string email)
+    {
+         var user = await _userManager.FindByEmailAsync(email);
+         var claims = await _userManager.GetClaimsAsync(user);
+         var roles = await _userManager.GetRolesAsync(user);
+        
+         // Claims para o JWT
+         claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+         claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+         claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+         claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+         claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+         foreach (var userRole in roles)
+         {
+             claims.Add(new Claim(ClaimTypes.Role, userRole));
+         }
+         
+         var identityClaims = new ClaimsIdentity();
+         identityClaims.AddClaims(claims);
+         
+         var tokenHandler = new JwtSecurityTokenHandler();
+         var tokenKey = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+         var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+         {
+             Issuer = _appSettings.Emissor,
+             Audience = _appSettings.ValidoEm,
+             Subject = identityClaims,
+             Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey),
+                 SecurityAlgorithms.HmacSha256Signature)
+         }); 
+         
+         var encodedToken = tokenHandler.WriteToken(token);
+
+         var response = new AdministratorResponseVM
+         {
+             AccessToken = encodedToken,
+             ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+             AdministratorToken = new AdministratorToken
+             {
+                 Id = user.Id,
+                 Email = user.Email,
+                 Claims = claims.Select(c => new AdministratorClaim { Type = c.Type, Value = c.Value })
+             }
+         };
+         
+         return response;
+    }
+    
+    private static long ToUnixEpochDate(DateTime date)
+        => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
 }
