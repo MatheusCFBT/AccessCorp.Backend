6ce1874ca159d0459c0d97b4bcc82f3b7653d450
MatheusCFBT <matheusterzini@gmail.com>
2025-03-10 19:45:40 -0300

    Add Doorman controller for actions

--- AccessCorp.Identity/AccessCorp.Application/Entities/DoormanVM.cs
+++ AccessCorp.Identity/AccessCorp.Application/Entities/DoormanVM.cs
@@ -34,3 +34,17 @@ public class DoormanLoginVM
     [StringLength(100, MinimumLength = 6, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
     public string Senha { get; set; } 
 }
+public class DoormanUpdateVM
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório ")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório ")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
+    public string UserName { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório ")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
+    public string Password { get; set; }
+}
