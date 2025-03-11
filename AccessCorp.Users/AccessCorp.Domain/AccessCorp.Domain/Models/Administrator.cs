c7a5c96026af0ca819bd3498aa11401a0ea9ea28
MatheusCFBT <matheusterzini@gmail.com>
2025-03-10 21:31:00 -0300

    Configure microservice

---
+++ AccessCorp.Users/AccessCorp.Domain/AccessCorp.Domain/Models/Administrator.cs
@@ -0,0 +1,9 @@
+﻿namespace AccessCorp.Domain.Models;
+
+public class Administrator
+{
+    public int Id { get; set; }
+    public string NomeCompleto { get; set; }
+    public string Cep { get; set; }
+    public Guid IdentityId { get; set; }
+}
---
+++ AccessCorp.Users/AccessCorp.Infra/AccessCorp.Infra/Context/AccessCorpUsersDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AccessCorp.Infra.Context;
+
+public class AccessCorpUsersDbContext : DbContext
+{
+    public AccessCorpUsersDbContext(DbContextOptions<AccessCorpUsersDbContext> options) : base(options) { }
+    
+    Db
+}
