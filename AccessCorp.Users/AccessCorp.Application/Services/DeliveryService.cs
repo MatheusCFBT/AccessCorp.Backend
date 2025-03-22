41421820a33d1e7616fc8bb21b8e1f1addabc22e
MatheusCFBT <matheusterzini@gmail.com>
2025-03-22 15:51:04 -0300

    add delivery and actions

---
+++ AccessCorp.Users/AccessCorp.Application/Services/DeliveryService.cs
@@ -0,0 +1,52 @@
+﻿using AccessCorpUsers.Application.Entities;
+using AccessCorpUsers.Application.Interfaces;
+using AccessCorpUsers.Domain.Interfaces;
+using AutoMapper;
+
+namespace AccessCorpUsers.Application.Services
+{
+    public class DeliveryService : IDeliveryService
+    {
+        private readonly IDeliveryRepository _deliveryRepository;
+        private readonly IDoormanRepository _doormanRepository;
+        private readonly IMapper _mapper;
+
+        public DeliveryService(IDeliveryRepository deliveryRepository, IDoormanRepository doormanRepository, IMapper mapper)
+        {
+            _deliveryRepository = deliveryRepository;
+            _doormanRepository = doormanRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result> ViewAllDeliveries(string email)
+        {
+            var requestDoorman = await _doormanRepository.GetDoormanByEmail(email);
+
+            var deliveries = await _deliveryRepository.GetDeliveriesByCep(requestDoorman.Cep);
+
+            var deliveryVM = _mapper.Map<List<DeliveryVM>>(deliveries);
+
+            return Result.Ok(deliveryVM);
+        }
+
+        Task<Result> IDeliveryService.RegisterDelivery(DeliveryVM request)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Result> IDeliveryService.UpdateDelivery(string email, DeliveryVM request)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<DeliveryVM> IDeliveryService.ViewDeliveryById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> ExcludeDelivery(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AccessCorp.Users/AccessCorp.WebApi/Controllers/DeliveryController.cs
@@ -0,0 +1,27 @@
+﻿using AccessCorpUsers.Application.Interfaces;
+using AccessCorpUsers.WebApi.Extensions;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessCorpUsers.WebApi.Controllers
+{
+
+    [ApiVersion("1.0")]
+    [ClaimsAuthorize("Permission", "FullAccess"), Route("users/v1/delivery")]
+    public class DeliveryController : MainController
+    {
+        private readonly IDeliveryService _deliveryService;
+
+        public DeliveryController(IDeliveryService deliveryService)
+        {
+            _deliveryService = deliveryService;   
+        }
+
+        public async Task<ActionResult> GetAllDeliveries()
+        {
+            var userId = GetUserId(HttpContext.User);
+            
+
+        }
+    }
+}
